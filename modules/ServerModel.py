500e44ee6f0eff2822cc83c9e2472129baa4cfb2
sciueferrara <antonioferrara94@icloud.com>
2020-05-19 18:38:18 +0200

    first implementation

--- modules/ServerModel.py
+++ modules/ServerModel.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class ServerModel:
     def __init__(self, n_items, n_factors):
-        self.item_vecs = np.random.randn(n_items, n_factors)/10
+        self.item_vecs = sp.sparse.csr_matrix(np.random.randn(n_items, n_factors)/10)
         self.item_bias = np.random.randn(n_items)/10
         self.item_vecs_delta = np.copy(self.item_vecs)
         self.item_bias_delta = np.copy(self.item_bias)
