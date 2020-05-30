2ad5f8b3db8b8ec0d4d12007fe762fb1c6a0ada9
sciueferrara <antonioferrara94@icloud.com>
2020-05-30 12:33:34 +0200

    first implementation

--- modules/Worker.py
+++ modules/Worker.py
@@ -39,8 +39,8 @@ class Worker(multiprocessing.Process):
             self.clients[next_task].model.user_vec = sp.sparse.csr_matrix(sp.sparse.linalg.spsolve(YTCuY + reg, Yt.dot(self.clients[next_task].Cu).dot(
                 sp.sparse.csr_matrix(np.ones(len(self.clients[next_task].train_set))).T)))
 
+            print(len(self.clients[next_task].train_set))
             for i in range(len(self.clients[next_task].train_set)):
-                print('calcolo')
                 grad = self.lr * 2 * (
                         sp.sparse.csr_matrix(self.clients[next_task].train_set[i]) - self.clients[next_task].model.user_vec.dot(
                     self.starting_model.item_vecs[i].T)) * self.clients[next_task].model.user_vec
