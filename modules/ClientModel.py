e8488f4ed7c9d1f84bf1a4af2118819467724cfd
sciueferrara <antonioferrara94@icloud.com>
2020-05-30 12:21:24 +0200

    first implementation

--- modules/ClientModel.py
+++ modules/ClientModel.py
@@ -9,7 +9,7 @@ class ClientModel:
         self.user_vec = sp.sparse.csr_matrix(np.random.randn(n_factors) / 10)
 
     def predict(self, server_model):
-        return np.dot(server_model.item_vecs, self.user_vec.T) + server_model.item_bias
+        return np.dot(server_model.item_vecs, self.user_vec.T) + server_model.item_bias[:]
 
     def predict_one(self, i, server_model):
         return np.dot(server_model.item_vecs[i], self.user_vec) + server_model.item_bias[i]
